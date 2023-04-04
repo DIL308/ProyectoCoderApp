241c45a5789e6190f1c4f701add649da8764a006
MauriiAC <mauriciocuello91@gmail.com>
2023-04-04 18:24:23 -0300

    herencia de templates y admin de django

--- ProyectoCoderApp/AppCoder/models.py
+++ ProyectoCoderApp/AppCoder/models.py
@@ -6,11 +6,17 @@ class Curso(models.Model):
   nombre = models.CharField(max_length=50)
   camada = models.IntegerField()
 
+  def __str__(self):
+    return f'{self.nombre} - {self.camada}'
+
 class Estudiante(models.Model):
 
   nombre = models.CharField(max_length=50)
   apellido = models.CharField(max_length=50)
   email = models.EmailField(max_length=254)
+
+  def __str__(self):
+    return f'{self.nombre}'  
 
 class Profesor(models.Model):
 
@@ -18,10 +24,12 @@ class Profesor(models.Model):
   apellido = models.CharField(max_length=50)
   email = models.EmailField(max_length=254)
   profesion = models.CharField(max_length=50)
+  cursos = models.ManyToManyField(Curso)
 
 class Entregable(models.Model):
 
   nombre = models.CharField(max_length=50)
   fecha_entrega = models.DateField()
   entregado = models.BooleanField()
+  estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
 
--- ProyectoCoderApp/AppCoder/urls.py
+++ ProyectoCoderApp/AppCoder/urls.py
@@ -5,9 +5,9 @@ from AppCoder.views import *
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
     path('lista-cursos/', lista_cursos),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('', inicio, name="Inicio"),
+    path('cursos/', cursos, name="Cursos"),
+    path('profesores/', profesores, name="Profesores"),
+    path('estudiantes/', estudiantes, name="Estudiantes"),
+    path('entregables/', entregables, name="Entregables"),
 ]
